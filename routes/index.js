4b64b5104a3776ef828f6dffdd0a958120a23dd4
cristiano200-ccof <ccof2009@gmail.com>
2026-09-24 15:41:21 -0300

    tutorial feito

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,9 @@
-//oi
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index',{title: 'Home'});
+});
+
+module.exports = router;
